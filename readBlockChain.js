adc74bdbe8a56c7607350c351a8a24843c03a74a
arman <fakidarman@gmail.com>
2024-01-01 03:23:28 -0500

    get a particular real ethereum address balance

--- readBlockChain.js
+++ readBlockChain.js
@@ -7,6 +7,16 @@ const provider = new JsonRpcProvider(`https://mainnet.infura.io/v3/b88ece4a38fe4
 const queryBlockchain = async () => {
     const block = await provider.getBlockNumber();
     console.log(block);
+
+    const balance = await provider.getBalance("0xbEAb388DAa2B714e4c7C105811a86756Dc274115");
+
+    // Convert the balance to a human-readable string (in Ether)
+    const balanceEther = ethers.utils.formatEther(balance);
+    console.log(balanceEther);
+
+    const balanceWei = ethers.utils.parseEther(balanceEther);
+
+    console.log(balanceWei);
 }
 
 queryBlockchain();
